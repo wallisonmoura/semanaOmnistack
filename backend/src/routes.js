a36d3d043fcf6996ed8f31409bedd80163843b60
wallisonmoura <wallison_cedaspy@hotmail.com>
2020-03-27 23:30:05 -0300

    criação e organização das validações com celebrate

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,62 +5,31 @@ const OngController = require("./controllers/OngController");
 const IncidentController = require("./controllers/IncidentController");
 const ProfileController = require("./controllers/ProfileController");
 const SessionController = require("./controllers/SessionController");
+const {
+  validLogIn,
+  validOngCreate,
+  validOngProfile,
+  validIncidentsListPage,
+  validIncidentsCreate,
+  validIncidentsDelete
+} = require("./utils/routeValidations");
 
 const router = express.Router();
 
-router.post("/session", SessionController.create);
+router.post("/session", validLogIn(), SessionController.create);
 
 router.get("/ongs", OngController.read);
-router.post(
-  "/ongs",
-  celebrate({
-    [Segments.BODY]: Joi.object().keys({
-      name: Joi.string().required(),
-      email: Joi.string()
-        .required()
-        .email(),
-      whatsapp: Joi.string()
-        .required()
-        .min(10)
-        .max(11),
-      city: Joi.string().required(),
-      uf: Joi.string()
-        .required()
-        .length(2)
-    })
-  }),
-  OngController.create
-);
+router.post("/ongs", validOngCreate(), OngController.create);
 
-router.get(
-  "/profile",
-  celebrate({
-    [Segments.HEADERS]: Joi.object({
-      authorization: Joi.string().required()
-    }).unknown()
-  }),
-  ProfileController.read
-);
+router.get("/profile", validOngProfile(), ProfileController.read);
 
-router.get(
-  "/incidents",
-  celebrate({
-    [Segments.QUERY]: Joi.object().keys({
-      page: Joi.number()
-    })
-  }),
-  IncidentController.read
-);
+router.get("/incidents", validIncidentsListPage(), IncidentController.read);
 
-router.post("/incidents", IncidentController.create);
+router.post("/incidents", validIncidentsCreate(), IncidentController.create);
 
 router.delete(
   "/incidents/:id",
-  celebrate({
-    [Segments.PARAMS]: Joi.object().keys({
-      id: Joi.number().required()
-    })
-  }),
+  validIncidentsDelete(),
   IncidentController.delete
 );
 
